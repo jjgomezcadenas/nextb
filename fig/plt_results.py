c41eb86427bcaadf506e3e2175afa8beeb9db5b4
josh <jrenner@nextp12.ific.uv.es>
2015-02-19 12:10:31 +0100

    Updated draft, B-field study only

--- fig/plt_results.py
+++ fig/plt_results.py
@@ -46,24 +46,48 @@ fig.set_figheight(5.0);
 fig.set_figwidth(7.5);
 plt.plot(bvals_p5, eff80_p5, '-', color='blue', label='5 atm');
 plt.plot(bvals_p5, eff80_p5, '.', color='blue');
-plt.plot(bvals_p5, eff90_p5, '-.', color='blue');
-plt.plot(bvals_p5, eff90_p5, '.', color='blue');
+#plt.plot(bvals_p5, eff90_p5, '-.', color='blue');
+#plt.plot(bvals_p5, eff90_p5, '.', color='blue');
 
 plt.plot(bvals_p10, eff80_p10, '-', color='green', label='10 atm');
 plt.plot(bvals_p10, eff80_p10, '.', color='green');
-plt.plot(bvals_p10, eff90_p10, '-.', color='green');
-plt.plot(bvals_p10, eff90_p10, '.', color='green');
+#plt.plot(bvals_p10, eff90_p10, '-.', color='green');
+#plt.plot(bvals_p10, eff90_p10, '.', color='green');
 
 plt.plot(bvals_p15, eff80_p15, '-', color='red', label='15 atm');
 plt.plot(bvals_p15, eff80_p15, '.', color='red');
-plt.plot(bvals_p15, eff90_p15, '-.', color='red');
+#plt.plot(bvals_p15, eff90_p15, '-.', color='red');
+#plt.plot(bvals_p15, eff90_p15, '.', color='red');
+
+lnd = plt.legend(loc=4,frameon=False,handletextpad=0);
+plt.xlabel("Magnetic Field (T)");
+plt.ylabel("Signal Efficiency");
+plt.title("80% Background Rejection");
+
+# Print the plot.
+fn_plt = "eff_vs_b_80.pdf";
+plt.savefig(fn_plt, bbox_inches='tight');
+plt.close();
+
+# Create the plot.
+fig = plt.figure(2);
+fig.set_figheight(5.0);
+fig.set_figwidth(7.5);
+plt.plot(bvals_p5, eff90_p5, '-', color='blue', label='5 atm');
+plt.plot(bvals_p5, eff90_p5, '.', color='blue');
+
+plt.plot(bvals_p10, eff90_p10, '-', color='green', label='10 atm');
+plt.plot(bvals_p10, eff90_p10, '.', color='green');
+
+plt.plot(bvals_p15, eff90_p15, '-', color='red', label='15 atm');
 plt.plot(bvals_p15, eff90_p15, '.', color='red');
 
 lnd = plt.legend(loc=4,frameon=False,handletextpad=0);
 plt.xlabel("Magnetic Field (T)");
 plt.ylabel("Signal Efficiency");
+plt.title("90% Background Rejection");
 
 # Print the plot.
-fn_plt = "eff_vs_b.pdf";
+fn_plt = "eff_vs_b_90.pdf";
 plt.savefig(fn_plt, bbox_inches='tight');
 plt.close();
