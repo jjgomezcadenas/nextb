aacb173e9959faf97ec0248c98319488a5a6ac4f
josh <jrenner@nextp12.ific.uv.es>
2015-03-10 18:36:14 +0100

    Updated with fixed-filter xenon results

--- fig/plt_results.py
+++ fig/plt_results.py
@@ -18,6 +18,16 @@ l_pressure = restbl[:,0];
 l_bfield = restbl[:,1];
 l_eff80 = restbl[:,2];
 l_eff90 = restbl[:,3];
+l_eff80_fixed = restbl[:,4];
+l_eff90_fixed = restbl[:,5];
+
+s_restbl = np.loadtxt("results_SeF6.dat");
+l_spressure = s_restbl[:,0];
+l_sbfield = s_restbl[:,1];
+l_seff80 = s_restbl[:,2];
+l_seff90 = s_restbl[:,3];
+l_seff80_fixed = s_restbl[:,4];
+l_seff90_fixed = s_restbl[:,5];
 
 sevsbtbl1 = np.loadtxt("sevsb_tbl1.dat");
 l_sevsb1_se = sevsbtbl1[:,3];
@@ -38,22 +48,54 @@ l_sevsb3_bb = [];
 for fbb in l_sevsb3_bb0: l_sevsb3_bb.append(1-fbb);
     
 # Prepare the plotted quantities.
-bvals_p5 = []; eff80_p5 = []; eff90_p5 = [];
-bvals_p10 = []; eff80_p10 = []; eff90_p10 = [];
-bvals_p15 = []; eff80_p15 = []; eff90_p15 = [];
-for pr,bfield,eff80,eff90 in zip(l_pressure,l_bfield,l_eff80,l_eff90):
+bvals_p5 = []; eff80_p5 = []; eff90_p5 = []; eff80_fp5 = []; eff90_fp5 = [];
+bvals_p10 = []; eff80_p10 = []; eff90_p10 = []; eff80_fp10 = []; eff90_fp10 = [];
+bvals_p15 = []; eff80_p15 = []; eff90_p15 = []; eff80_fp15 = []; eff90_fp15 = [];
+for pr,bfield,eff80,eff90,eff80_fixed,eff90_fixed in zip(l_pressure,l_bfield,l_eff80,l_eff90,l_eff80_fixed,l_eff90_fixed):
     if(pr == 5):
         bvals_p5.append(bfield);
         eff80_p5.append(eff80);
         eff90_p5.append(eff90);
+        eff80_fp5.append(eff80_fixed);
+        eff90_fp5.append(eff90_fixed);
     elif(pr == 10):
         bvals_p10.append(bfield);
         eff80_p10.append(eff80); 
         eff90_p10.append(eff90);
+        eff80_fp10.append(eff80_fixed);
+        eff90_fp10.append(eff90_fixed);
     elif(pr == 15):
         bvals_p15.append(bfield);
         eff80_p15.append(eff80);
         eff90_p15.append(eff90);
+        eff80_fp15.append(eff80_fixed);
+        eff90_fp15.append(eff90_fixed);
+    else:
+        print "WARNING: unexpected pressure = {0}".format(pr);
+
+# Prepare the plotted quantities.
+bvals_sp5 = []; eff80_sp5 = []; eff90_sp5 = []; eff80_fsp5 = []; eff90_fsp5 = [];
+bvals_sp10 = []; eff80_sp10 = []; eff90_sp10 = []; eff80_fsp10 = []; eff90_fsp10 = [];
+bvals_sp15 = []; eff80_sp15 = []; eff90_sp15 = []; eff80_fsp15 = []; eff90_fsp15 = [];
+for pr,bfield,eff80,eff90,eff80_fixed,eff90_fixed in zip(l_spressure,l_sbfield,l_seff80,l_seff90,l_seff80_fixed,l_seff90_fixed):
+    if(pr == 5):
+        bvals_sp5.append(bfield);
+        eff80_sp5.append(eff80);
+        eff90_sp5.append(eff90);
+        eff80_fsp5.append(eff80_fixed);
+        eff90_fsp5.append(eff90_fixed);
+    elif(pr == 10):
+        bvals_sp10.append(bfield);
+        eff80_sp10.append(eff80);
+        eff90_sp10.append(eff90);
+        eff80_fsp10.append(eff80_fixed);
+        eff90_fsp10.append(eff90_fixed);
+    elif(pr == 15):
+        bvals_sp15.append(bfield);
+        eff80_sp15.append(eff80);
+        eff90_sp15.append(eff90);
+        eff80_fsp15.append(eff80_fixed);
+        eff90_fsp15.append(eff90_fixed);
     else:
         print "WARNING: unexpected pressure = {0}".format(pr);
 
@@ -61,20 +103,26 @@ for pr,bfield,eff80,eff90 in zip(l_pressure,l_bfield,l_eff80,l_eff90):
 fig = plt.figure(1);
 fig.set_figheight(5.0);
 fig.set_figwidth(7.5);
-plt.plot(bvals_p5, eff80_p5, '-', color='blue', label='5 atm');
+plt.plot(bvals_p5, eff80_p5, '-', color='blue', label='Xe, 5 atm');
 plt.plot(bvals_p5, eff80_p5, '.', color='blue');
-#plt.plot(bvals_p5, eff90_p5, '-.', color='blue');
-#plt.plot(bvals_p5, eff90_p5, '.', color='blue');
+plt.plot(bvals_p5, eff80_fp5, '-.', color='blue');
+plt.plot(bvals_p5, eff80_fp5, '.', color='blue');
 
-plt.plot(bvals_p10, eff80_p10, '-', color='green', label='10 atm');
+
+plt.plot(bvals_p10, eff80_p10, '-', color='green', label='Xe, 10 atm');
 plt.plot(bvals_p10, eff80_p10, '.', color='green');
-#plt.plot(bvals_p10, eff90_p10, '-.', color='green');
-#plt.plot(bvals_p10, eff90_p10, '.', color='green');
+plt.plot(bvals_p10, eff80_fp10, '-.', color='green');
+plt.plot(bvals_p10, eff80_fp10, '.', color='green');
 
-plt.plot(bvals_p15, eff80_p15, '-', color='red', label='15 atm');
+plt.plot(bvals_sp10, eff80_sp10, '-', color='orange', label='SeF$_6$, 10 atm');
+plt.plot(bvals_sp10, eff80_sp10, '.', color='orange');
+plt.plot(bvals_sp10, eff80_fsp10, '-.', color='orange');
+plt.plot(bvals_sp10, eff80_fsp10, '.', color='orange');
+
+plt.plot(bvals_p15, eff80_p15, '-', color='red', label='Xe, 15 atm');
 plt.plot(bvals_p15, eff80_p15, '.', color='red');
-#plt.plot(bvals_p15, eff90_p15, '-.', color='red');
-#plt.plot(bvals_p15, eff90_p15, '.', color='red');
+plt.plot(bvals_p15, eff80_fp15, '-.', color='red');
+plt.plot(bvals_p15, eff80_fp15, '.', color='red');
 
 lnd = plt.legend(loc=4,frameon=False,handletextpad=0);
 plt.xlabel("Magnetic Field (T)");
@@ -90,14 +138,25 @@ plt.close();
 fig = plt.figure(2);
 fig.set_figheight(5.0);
 fig.set_figwidth(7.5);
-plt.plot(bvals_p5, eff90_p5, '-', color='blue', label='5 atm');
+plt.plot(bvals_p5, eff90_p5, '-', color='blue', label='Xe, 5 atm');
 plt.plot(bvals_p5, eff90_p5, '.', color='blue');
+plt.plot(bvals_p5, eff90_fp5, '-.', color='blue');
+plt.plot(bvals_p5, eff90_fp5, '.', color='blue');
 
-plt.plot(bvals_p10, eff90_p10, '-', color='green', label='10 atm');
+plt.plot(bvals_p10, eff90_p10, '-', color='green', label='Xe, 10 atm');
 plt.plot(bvals_p10, eff90_p10, '.', color='green');
+plt.plot(bvals_p10, eff90_fp10, '-.', color='green');
+plt.plot(bvals_p10, eff90_fp10, '.', color='green');
 
-plt.plot(bvals_p15, eff90_p15, '-', color='red', label='15 atm');
+plt.plot(bvals_sp10, eff90_sp10, '-', color='orange', label='SeF$_6$, 10 atm');
+plt.plot(bvals_sp10, eff90_sp10, '.', color='orange');
+plt.plot(bvals_sp10, eff90_fsp10, '-.', color='orange');
+plt.plot(bvals_sp10, eff90_fsp10, '.', color='orange');
+
+plt.plot(bvals_p15, eff90_p15, '-', color='red', label='Xe, 15 atm');
 plt.plot(bvals_p15, eff90_p15, '.', color='red');
+plt.plot(bvals_p15, eff90_fp15, '-.', color='red');
+plt.plot(bvals_p15, eff90_fp15, '.', color='red');
 
 lnd = plt.legend(loc=4,frameon=False,handletextpad=0);
 plt.xlabel("Magnetic Field (T)");
